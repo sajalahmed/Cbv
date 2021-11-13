700779033260968bd3793f68f70636f32a3f94f8
Sajal Ahmed <sazal.me@gmail.com>
2021-11-14 00:16:13 +0600

    listView

--- Core/books/urls.py
+++ Core/books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndexView, BookDetailsView
+from .views import IndexView, BookDetailsView, BookListView
 
 app_name = 'books'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="books"),
+    #path('', IndexView.as_view(), name="books"),
+    path('', BookListView.as_view(), name="book_list"),
     path('<slug:slug>/', BookDetailsView.as_view(), name="book-detail"),
 ]
--- Core/books/views.py
+++ Core/books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.utils import timezone
 
 from .models import Book
@@ -25,3 +26,25 @@ class BookDetailsView(DetailView):
         books = Book.objects.filter(slug=self.kwargs.get('slug'))
         context['time'] = timezone.now()
         return context
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book/list.html"
+    context_object_name = "books"     #default object_list
+    paginate_by = 4
+    #queryset = Book.objects.all()[:2]
+    #or method
+    def get_queryset(self):
+        return Book.objects.all()[:2]
+
+
+class BookListViewExtra(ListView):
+    model = Book
+    template_name = "book/list.html"
+    context_object_name = "books"     #default object_list
+    paginate_by = 4
+    #queryset = Book.objects.all()[:2]
+    #or method
+    def get_queryset(self, *args, **kwargs):
+        return Book.objects.filter(title__icontains=self.kwargs.get('title')) #__icontains for case sensitive search
