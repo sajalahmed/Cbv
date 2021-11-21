0172963f84d696217c9ff385ebdf3f0a8841414d
Sajal Ahmed <sazal.me@gmail.com>
2021-11-21 21:42:23 +0600

    args and kwargs

--- Core/users/urls.py
+++ Core/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, userTest
 
 app_name = 'users'
 
 urlpatterns = [
     path('users', UserView.as_view(), name="users"),
+    path('test', userTest, name="user_test")
 ]
--- Core/users/views.py
+++ Core/users/views.py
@@ -6,3 +6,30 @@ from django.views.generic import TemplateView
 
 class UserView(TemplateView):
     template_name = "users.html"
+
+
+"""
+* => single asterisk means unpack a list
+** => double asterisk means uppack a dictornary 
+"""
+
+
+def add_number(a, b, c):
+    print(a, b, c)
+
+def __args(*args): #args work like touple and **kwargs work like dict
+    print(args)
+
+def __kwargs(**kwargs):
+    print(kwargs)
+
+def userTest(request):
+    b =[3, 3, 3]
+    add_number(*b)
+    __args(1, 3, 5)
+    val = {'a': 1, 'b': 2}
+    __kwargs(a = 1, b = 2)
+    #or
+    __kwargs(**val)
+
+    return render(request, 'test.html')
